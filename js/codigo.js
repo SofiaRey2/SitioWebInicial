467b604be17f2bc222771542496534a9089fb9b6
SofiaRey2 <sofiamarlene168@gmail.com>
2022-06-15 18:05:23 -0300

    prueba

---
+++ js/codigo.js
@@ -0,0 +1,11 @@
+function PreguntarNombreYApellido(){
+    var nombre=prompt("Ingresa tu nombre:");
+    var apellido=prompt("Ingresa tu apellido:");
+    alert(`Hola tu nombre es ${nombre} y tu apellido es ${apellido}`);
+}
+function ponerEnMayusculas(){
+    var datosPersonales=document.getElementById("datos");
+    datosPersonales.innerHTML="DATOS PERSONALES";
+    datosPersonales.style.background="white";
+}
+vn
